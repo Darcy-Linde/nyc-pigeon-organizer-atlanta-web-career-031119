539c61fd9a5a5484c146d3fc9ee34d615c918c66
Darcy Linde <lindedarcy@gmail.com>
2019-03-03 19:28:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,29 +17,19 @@
 #   }
 # }
 
-def nyc_pigeon_organizer(data)
-  names_array = []
-  data.each do |attribute, values|
-    values.each do |key, names|
-      names.each do |name|
-        if !names_array.include?(name)
-          names_array << name
-        end
-      end
-    end
-  end
+# def nyc_pigeon_organizer(data)
+#   names_array = []
+#   data.each do |attribute, values|
+#     values.each do |key, names|
+#       names.each do |name|
+#         if !names_array.include?(name)
+#           names_array << name
+#         end
+#       end
+#     end
+#   end
   
-  pidgeon_list = {}
-  names_array.each do |name|
-    pidgeon_list[name] = {color: [], gender: [], lives: []}
-  end
-  # pidgeon_list.each do |name, value|
-  #   data[value].each do |key, info|
-  #     if info.include?(name)
-  #       pidgeon_list[name] << key.to_s
-  #     end
-  #   end
-  # end
+
   
   
   pidgeon_list
